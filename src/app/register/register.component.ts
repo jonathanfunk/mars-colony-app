fe3d59a753ccf11d91be8fb4df97d63ee2f553d8
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-16 20:47:58 -0800

    register form now functioning

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,60 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl  } from '@angular/forms';
-import { NewColonist, Job } from '../models';
+import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl  } from '@angular/forms';//This line imports form functionalities
+import { NewColonist, Job } from '../models';//This line will import classes which will be used in this component
 import JobsService from '../services/jobs.service';
 
-
-
-
 @Component({
- selector: 'app-register',
- templateUrl: './register.component.html',
- styleUrls: ['./register.component.css'],
- providers: [JobsService]
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  providers: [JobsService]
 })
 export class RegisterComponent implements OnInit {
 
- marsJobs: Job[];
- registerForm: FormGroup;
+  colonist: NewColonist;
+  marsJobs: Job[];
+  registerForm: FormGroup;
 
- NO_JOB_SELECTED = '(none)';
+  NO_JOB_SELECTED = '(none)';
 
- constructor(public jobService: JobsService,
-             /* private formBuilder: FormBuilder */) {
+  constructor(public jobService: JobsService,
+              private formBuilder: FormBuilder) {
 
-     jobService.getJobs().subscribe((jobs) => {
-       this.marsJobs = jobs;
-     }, (err) => {
-       console.log(err);
-     });
+      jobService.getJobs().subscribe((jobs) => {
+        this.marsJobs = jobs;
+      }, (err) => {
+        console.log(err);
+      });
 
- }
+  }
 
- cantBe(value: string): ValidatorFn {
-   return (control: AbstractControl): {[key: string]: any} => {
-     return control.value === value ? {'cant be value': { value }} : null;
-   };
- }
+  cantBe(value: string): ValidatorFn {
+     return (control: AbstractControl): {[key: string]: any} => {
+        return control.value === value ? {'cant be value': { value }} : null;
+    };
+  }
 
- ngOnInit() {
+  tooOld(value: number): ValidatorFn {
+     return (control: AbstractControl): {[key: string]: any} => {
+        return control.value > 100 ? {'too old': { value }} : null;
+    };
+  }
 
-   this.registerForm = new FormGroup({
-     name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
-     age: new FormControl('', [Validators.required, Validators.minLength(1)]),
-     job_id: new FormControl(this.NO_JOB_SELECTED, [this.cantBe(this.NO_JOB_SELECTED)])
-   });
+  ngOnInit() {
 
- }
+    //This is used to validate a form
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+      age: new FormControl('', [Validators.required, this.tooOld(100)]),
+      job_id: new FormControl(this.NO_JOB_SELECTED, [this.cantBe(this.NO_JOB_SELECTED)])
+    });
+  }
 
-   onSubmit(event) {
-     event.preventDefault();
-     if(this.registerForm.invalid){
-       console.log("Not working!")
-     } else {
-       const name = this.registerForm.get('name').value;
-       const age = this.registerForm.get('age').value;
-       const job_id = this.registerForm.get('job_id').value;
-       console.log("Okay, let's register this new colonist:", new NewColonist(name, age, job_id));
-     }
-   }
+  onSubmit(event) {
+    event.preventDefault();
+    if (this.registerForm.invalid) {
+      // The form is invalid...
+    } else {
+      const name = this.registerForm.get('name').value;
+      const age = this.registerForm.get('age').value;
+      const job_id = this.registerForm.get('job_id').value;
+
+      console.log('Ok, let\'s register this new colonist:', new NewColonist(name, age, job_id));
+    }
+  }
 }
